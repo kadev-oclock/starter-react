54353316ab73d7cbab6412fe84a2241c5764e596
kadev-oclock <karimaoudiaa@gmail.com>
2020-10-22 12:34:06 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//je veux une fonction sum qui prend 2 paramètre et me retournela somme de ces 2 paramètres 
+function sum(a, b) {
+    return a + b;
+}
+// function flecher idem que en haut 
+const sum =(a, b) => a + b;
